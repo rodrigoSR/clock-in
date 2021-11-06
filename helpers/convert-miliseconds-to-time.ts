d45ace63e01e1d80936d21474dd3bd61b7095d60
Rodrigo Rosa <rodrig0x@hotmail.com>
2021-11-06 15:01:14 -0300

    chore: add prettier

--- helpers/convert-miliseconds-to-time.ts
+++ helpers/convert-miliseconds-to-time.ts
@@ -1,11 +1,13 @@
 export function convertMilisecondsToTime(miliseconds: number): string {
-    const seconds = Math.floor(miliseconds / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
+  const seconds = Math.floor(miliseconds / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
 
-    const secondsString = seconds % 60 < 10 ? `0${seconds % 60}` : `${seconds % 60}`;
-    const minutesString = minutes % 60 < 10 ? `0${minutes % 60}` : `${minutes % 60}`;
-    const hoursString = hours % 60 < 10 ? `0${hours % 60}` : `${hours % 60}`;
+  const secondsString =
+    seconds % 60 < 10 ? `0${seconds % 60}` : `${seconds % 60}`;
+  const minutesString =
+    minutes % 60 < 10 ? `0${minutes % 60}` : `${minutes % 60}`;
+  const hoursString = hours % 60 < 10 ? `0${hours % 60}` : `${hours % 60}`;
 
-    return `${hoursString}:${minutesString}:${secondsString}`;
+  return `${hoursString}:${minutesString}:${secondsString}`;
 }
